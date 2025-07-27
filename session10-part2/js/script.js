ecd4efca4e81eb6febff53a29b718a6ab7f85ac9
NouranFayez <nouran_ahmed269@hotmail.com>
2025-07-27 22:36:34 +0300

    images of task

---
+++ session10-part2/js/script.js
@@ -0,0 +1,47 @@
+console.log('js')
+
+// var , let , const
+
+
+// camel case
+// var firstName = "Nouran"
+// var first_name = "Nouran"
+// var First_Name = "Nouran"
+
+/// data type ==>  string , number , boolean , undefined , null 
+
+var firstName = "Nouran"
+var middleName = "Ahmed"
+var lastName = "Fayez"
+
+var fullName = firstName+" " +middleName+" "+lastName
+var bio = "My name is"+ firstName+" " +middleName+" "+lastName
+// template string
+var bio = `My name is
+ ${firstName} ${middleName} ${lastName}`
+
+var age = 31
+
+var h = 20
+var k = 30
+var sum = h+k
+
+console.log(sum)
+
+// boolean ==. true , false
+
+var isGraduated = true
+
+var num
+/// js is case sensitive
+// console.log(Sum)
+
+console.log(num)
+
+var k = null
+
+var m = ""
+
+var p = prompt("enter your name")
+
+console.log(p)
