d532ecd6f08365569d68a835c27e28304b4ceae5
NouranFayez <nouran_ahmed269@hotmail.com>
2025-08-10 20:55:29 +0300

    session 13

--- session13/js/script.js
+++ session13/js/script.js
@@ -81,27 +81,90 @@ var users = [
 // console.log(users[1])
 // console.log(users[2])
 users.forEach((element , index , array)=>{
-    if(element.address == "October")
-        console.log(element)
+    // if(element.address == "October")
+        // console.log(element)
 })
 
 
 var element = users.find(element=>element.address=="October")
 var index = users.findIndex(element=>element.address=="October")
-console.log(element)
-console.log(index)
+// console.log(element)
+// console.log(index)
 
 
 
-var userName = prompt('enter userName')
-var userAddress = prompt('enter Address')
+// var userName = prompt('enter userName')
+// var userAddress = prompt('enter Address')
 
-users.push({userName , userAddress})
+// users.push({userName , userAddress})
 
-console.log(users)
-
-
+// console.log(users)
 
 
 
+///// function numberOfUsers() // 3
 
+/// function addUser() ==> name , id (unique) , balance
+
+// function editUserBalanceById() ==> id , newBalance
+
+// function deleteUserById() ==> id
+
+
+// console.table(users)
+
+var usersList =[]
+var addUser = ()=>{
+    var user = {
+        name : prompt('enter userName '),
+        id : prompt('enter user id'),
+        balance : prompt('enter user balance')
+    }
+    usersList.push(user)
+    console.table(usersList)
+
+}
+
+// addUser()
+// addUser()
+var users = [
+    {
+    name:"Nouran",
+    address : "Nasr city"
+},
+{
+    name : "Marwa",
+    address : "October"
+},
+{
+    name : "Ahmed" ,
+    address : "Maadi"
+}
+]
+
+var courses =['html' , 'css' , 'js']
+
+// console.log(courses.toString())
+// localStorage.setItem('firstName' , "Nouran")
+// console.log(JSON.stringify(users))
+
+
+// localStorage.removeItem('users')
+
+localStorage.setItem('users' , JSON.stringify(users))
+
+var usersLocalStorage = JSON.parse(localStorage.getItem('users'))
+
+usersLocalStorage.push({name : 'marwa'})
+
+console.log(usersLocalStorage)
+
+
+
+
+// https://owlcarousel2.github.io/OwlCarousel2/docs/api-options.html
+
+
+// tradex template ==> home & contact page
+// https://digitaltemplatemarket.com/wp/tradex-premium/contact/ 
+// https://digitaltemplatemarket.com/wp/tradex-premium/
